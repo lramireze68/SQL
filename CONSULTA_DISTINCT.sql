427c22c4198336de56a91862a4ec5ee3f2ede6cf
lramireze68 <liramirezes@uninpahu.edu.co>
2025-05-11 21:36:38 -0500

    Add files via upload

---
+++ CONSULTA_DISTINCT.sql
@@ -0,0 +1,72 @@
+-- Se obtienen las distintas Ciudades de Residencia sin repetir
+--IMAGEN Nº 1 SELECT DISTINCT Ciudad_de_Residencia
+SELECT DISTINCT Ciudad_de_Residencia
+FROM COLOMBIANOSENELEXTERIOR.dbo.Colombianos_registrados_en_el_exterior
+ORDER BY Ciudad_de_Residencia ASC;
+
+ 
+-- Se obtienen los distintos Grupos de edad sin repetir
+--IMAGEN Nº 2 SELECT DISTINCT Grupos de edad
+SELECT DISTINCT Grupo_edad
+FROM COLOMBIANOSENELEXTERIOR.dbo.Colombianos_registrados_en_el_exterior
+ORDER BY Grupo_edad ASC;
+ 
+-- Se obtienen las distintas Edades sin repetir
+--IMAGEN Nº 3 SELECT DISTINCT  edad
+SELECT DISTINCT Edad
+FROM COLOMBIANOSENELEXTERIOR.dbo.Colombianos_registrados_en_el_exterior
+ORDER BY Edad ASC;
+ 
+-- Se obtienen las distintas Áreas de Conocimiento sin repetir
+--IMAGEN Nº 4 SELECT DISTINCT  Área_Conocimiento
+SELECT DISTINCT Área_Conocimiento
+FROM COLOMBIANOSENELEXTERIOR.dbo.Colombianos_registrados_en_el_exterior
+ORDER BY Área_Conocimiento ASC;
+ 
+-- Se obtienen las distintas Sub Áreas de Conocimiento sin repetir
+--IMAGEN Nº 5 SELECT DISTINCT  SUB AREA DE CONOCIMIENTO
+SELECT DISTINCT Sub_Area_Conocimiento
+FROM COLOMBIANOSENELEXTERIOR.dbo.Colombianos_registrados_en_el_exterior
+ORDER BY Sub_Area_Conocimiento ASC;
+ 
+-- Se obtienen los distintos Niveles Académicos sin repetir
+--IMAGEN Nº 6 SELECT DISTINCT  NIVEL ACADEMICO
+SELECT DISTINCT Nivel_Académico
+FROM COLOMBIANOSENELEXTERIOR.dbo.Colombianos_registrados_en_el_exterior
+ORDER BY Nivel_Académico ASC;
+ 
+-- Se obtienen los distintos Estados civiles sin repetir
+--IMAGEN Nº 7 SELECT DISTINCT ESTADO CIVIL
+SELECT DISTINCT Estado_civil
+FROM COLOMBIANOSENELEXTERIOR.dbo.Colombianos_registrados_en_el_exterior
+ORDER BY Estado_civil ASC;
+ 
+-- Se obtienen los distintos Géneros sin repetir
+--IMAGEN Nº 8 SELECT DISTINCT GENERO
+SELECT DISTINCT Género
+FROM COLOMBIANOSENELEXTERIOR.dbo.Colombianos_registrados_en_el_exterior
+ORDER BY Género ASC;
+ 
+-- Se obtienen las distintas Etnias de la persona sin repetir
+--IMAGEN Nº 9 SELECT DISTINCT ETNIA DE LA PERSONA
+SELECT DISTINCT Etnia_de_la_persona
+FROM COLOMBIANOSENELEXTERIOR.dbo.Colombianos_registrados_en_el_exterior
+ORDER BY Etnia_de_la_persona ASC;
+ 
+-- Se obtienen las distintas Estaturas sin repetir
+--IMAGEN Nº 10 SELECT DISTINCT ESTATURA
+SELECT DISTINCT Estatura
+FROM COLOMBIANOSENELEXTERIOR.dbo.Colombianos_registrados_en_el_exterior
+ORDER BY Estatura ASC;
+ 
+-- Se obtienen las distintas Ciudades de Nacimiento sin repetir
+--IMAGEN Nº 11 SELECT DISTINCT ESTATURA
+SELECT DISTINCT Ciudad_de_Nacimiento
+FROM COLOMBIANOSENELEXTERIOR.dbo.Colombianos_registrados_en_el_exterior
+ORDER BY Ciudad_de_Nacimiento ASC;
+ 
+-- Se obtienen los distintos Países sin repetir
+--IMAGEN Nº 12 SELECT DISTINCT PAIS
+SELECT DISTINCT País
+FROM COLOMBIANOSENELEXTERIOR.dbo.Colombianos_registrados_en_el_exterior
+ORDER BY País ASC;
