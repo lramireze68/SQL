73a0a20e778c53d9f5ebbdcade821c2dc7b8cb62
lramireze68 <liramirezes@uninpahu.edu.co>
2025-05-12 22:04:31 -0500

    Update CONSULTA_ESTADISTICOS_AVG_STDEVP.sql

--- CONSULTA_ESTADISTICOS_AVG_STDEVP.sql
+++ CONSULTA_ESTADISTICOS_AVG_STDEVP.sql
@@ -33,7 +33,7 @@ Where Género = 'FEMENINO'
 --en el exterior es aproximadamente 45 años. la desviacion indica que las edades estan bastante dispersas,
 -- - La mayoría de las edades se encontrarán aproximadamente entre 29 y 61 años
 
- --IMAGEN Nº 5 AVG Y STDEVP ESTATURA GENERO MASCULINO
+ --IMAGEN Nº 5  AVG Y STDEVP ESTATURA GENERO MASCULINO
 SELECT avg(Estatura), stdevp(Estatura)
 FROM Colombianosenelexterior.dbo.Colombianos_registrados_en_el_exterior
 Where Género = 'MASCULINO'
@@ -42,7 +42,7 @@ Where Género = 'MASCULINO'
 --en el exterior es aproximadamente 170cm, los datos se encuentran algo dispersos
 -- la mayoria de las estaturas esta entre 157cm Y 183cm
 
-  --IMAGEN Nº 5 AVG Y STDEVP ESTATURA GENERO FEMENINO
+  --IMAGEN Nº 6 AVG Y STDEVP ESTATURA GENERO FEMENINO
 SELECT avg(Estatura), stdevp(Estatura)
 FROM Colombianosenelexterior.dbo.Colombianos_registrados_en_el_exterior
 Where Género = 'FEMENINO'
@@ -52,7 +52,7 @@ Where Género = 'FEMENINO'
 -- la mayoria de las estaturas esta entre 149cm y 169cm
  
  
- --IMAGEN Nº 6 AVG Y STDEVP PAIS
+ --IMAGEN Nº 7 AVG Y STDEVP PAIS
 SELECT avg(Edad), stdevp(Edad)
 FROM Colombianosenelexterior.dbo.Colombianos_registrados_en_el_exterior
 WHERE País = 'ESTADOS UNIDOS'
@@ -62,7 +62,7 @@ WHERE País = 'ESTADOS UNIDOS'
 --la desviacion indica que las edades estan bastante dispersas,
 -- - La mayoría de las edades se encontrarán aproximadamente entre 29 y 63 años
 
-  --IMAGEN Nº 7 AVG Y STDEVP ESTADO CIVIL
+  --IMAGEN Nº 8 AVG Y STDEVP ESTADO CIVIL
 SELECT avg(Edad), stdevp(Edad)
 FROM Colombianosenelexterior.dbo.Colombianos_registrados_en_el_exterior
 WHERE Estado_civil = 'SOLTERO'
